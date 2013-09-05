dcecb3cf0ece61dbd56de7c69a7f41bc765b958a
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-05 11:07:45 -0500

    added car.rb with init and info methods

---
+++ car.rb
@@ -0,0 +1,9 @@
+class Car
+    def initialize
+        @fuel = 10
+        @distance = 0
+    end
+    def get_info
+        "I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left."
+    end
+end
