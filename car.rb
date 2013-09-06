eabc1d7dfc24b67a65db107dafc76e94076b2e8a
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-06 00:01:02 -0500

    fixed car.rb

--- car.rb
+++ car.rb
@@ -6,19 +6,20 @@ class Car
     def get_info
         "I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left."
     end
-    def drive(distance)
-        if @fuel - distance / 20.0 > 0
-            @distance += distance
-            @fuel -= distance / 20.0
+    def drive(dist)
+        if @fuel - dist / 20.0 >= 0
+            @distance += dist
+            @fuel -= dist / 20.0
+            puts "You traveled #{dist} miles. There is #{@fuel} gallons left in your tank."
         else
-            @distance += distance / 20.0 #
+            @distance += @fuel * 20.0
             @fuel = 0
             puts "You've traveled #{@distance} miles and now you're out of gas!"
         end
     end
     def fuel_up
         fuel_needed = 10 - @fuel
-        cost = fuel_needed / 3.50
+        cost = fuel_needed * 3.50
         @fuel = 10
         puts "That'll cost you #{cost}"
     end
